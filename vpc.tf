4f02e9d3fb6f09aa0462777d411297a5821ba361
Dominic Dumrauf (MacBook Pro 2017) <dominic-git-macbook-pro-2017@dumrauf.de>
2020-02-15 20:26:17 +0000

    Resolving TF-UPGRADE-TODO comment

--- vpc.tf
+++ vpc.tf
@@ -80,17 +80,9 @@ resource "aws_security_group" "ssh_from_local" {
   }
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibilty in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = [local.local_ip_address]
   }
 }
